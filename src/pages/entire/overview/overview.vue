1470d0f7b7f87a0b0340b6813df03d4f9de17832
Furrya Black <furrya.black@gmail.com>
2023-07-13 00:05:34 +0400

    feat: add link to noiseless resume

--- src/pages/entire/overview/overview.vue
+++ src/pages/entire/overview/overview.vue
@@ -5,6 +5,14 @@
       <UI.ContactToMe />
       <UI.SummaryInfo />
 
+      <div class="flex flex-col items-center justify-center -mt-3">
+        <span class="italic font-semibold">Noiseless version of this resume</span>
+        
+        <SharedUI.NativeLink href="https://furryablack.github.io/noiseless-resume/">
+          https://furryablack.github.io/noiseless-resume/
+        </SharedUI.NativeLink>
+      </div>
+
       <UI.CareersList>
         <UI.CareerItem :career="lastCareer"  />
       </UI.CareersList>
@@ -31,11 +39,12 @@
 
 <script setup>
 import { ref, computed } from "vue";
+import { Shared } from '@shared'; 
 import { UI } from '@pages/entire/ui'; 
 import { careers as careersStatic } from './careers';
 
+const { UI: SharedUI } = Shared;
 const careers = ref(careersStatic.map((career, idx) => ({...career, id: +(`${(idx + 1)}${(idx + 2)}`) })))
-
 const filteredCareers = computed(() => careers.value.splice(1));
 const lastCareer = computed(() => careers.value[0]);
 </script>
